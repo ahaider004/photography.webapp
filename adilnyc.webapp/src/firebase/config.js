d934d14edee6852ebe55426831f3ec89c4cfa34b
Haroon Syed <63178444+Hxsyed@users.noreply.github.com>
2022-10-25 09:01:52 -0500

    hid the api keys and some other info, works local

--- adilnyc.webapp/src/firebase/config.js
+++ adilnyc.webapp/src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
 import 'firebase/compat/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyBjLRsq0LXiPVGBiFutMyOgrg8s5HMtprQ",
+    apiKey: process.env.REACT_APP_apiKey,
     authDomain: "adil-rahman.firebaseapp.com",
     projectId: "adil-rahman",
     storageBucket: "adil-rahman.appspot.com",
-    messagingSenderId: "755282142431",
-    appId: "1:755282142431:web:be1fe2c37dd0d2bfd2f56a"
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
